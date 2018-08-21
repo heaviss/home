bf02edf768d7fea65c706399bb1c872bf1597801
Vladimir <31631@rambler.ru>
2018-08-21 12:29:02 +0700

    update self-info

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,18 @@
         <div class="card">
             <div class="card-image">
               <figure class="image is-4by3">
-                <img src="~assets/photo.png" alt="Placeholder image">
+                <img src="~assets/photo.png" alt="Где фотка, ёпта?">
               </figure>
             </div>
             <div class="card-content">
               <p class="title">Привет, я Вова Серёгин.</p>
-              <p>Я учусь программировать на python (django) и javascript (vue.js).</p>
-              <p>Ещё мне интересны дизайн, редактура, устройство людей, рациональное мышление и сочинение музыки.</p>
-              <p>Веду с Верой Иноземцевой <a href="https://t.me/listen2it">канал в телеграме</a>, где она читает вслух клёвые статьи. Ещё иногда пишу в <a href="https://t.me/rdpms">блог</a>.</p>
+              <p>Я — «немного».</p>
+              <p>Немного программирую на python (django) и javascript (vue.js). Мой <a href="https://github.com/heaviss/ransomnote">ransomnote</a> пишет буквы, будто они вырезаны из газеты.</p>
+              <p>Немного сочиняю музыку, учусь играть на гитаре и губной гармошке.</p>
+              <p>Немного пишу (в <a href="https://t.me/rdpms">телеграм-канал</a>, занял 9 место на <a href="https://challenge.glvrd.ru/mtv.html">вызове Главреда</a>).</p>
+              <p>Много читаю, потому что люблю (45 книг в 2016, <a href="telegra.ph/thisage--26-02-19>"31 в 2017</a>). Читаю про разработку, дизайн, редактуру, устройство людей, рациональное мышление и сочинение музыки. И фантастику.</p>
+              <p>Веду с Верой Иноземцевой <a href="https://t.me/listen2it">канал «Слушай чё»</a>, где она читает вслух клёвые статьи.</p>
+              <p>Сейчас работаю безопасником в ЦФТ.</p>
             </div>
             <footer class="card-footer">
               <p class="card-footer-item">
