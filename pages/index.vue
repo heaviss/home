4324258bc53a276bc13082f914603588cdec6893
Vladimir Seregin <31631@rambler.ru>
2019-07-18 18:03:10 +0700

    Add css grids (#2)

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,27 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="columns is-centered">
-        <div class="column is-6">
-          <div class="card">
-            <div class="card-image">
-              <figure class="image is-4by3">
-                <img src="~assets/photo.png" alt="Где фотка, ёпта?">
-              </figure>
-            </div>
-            <div class="card-content">
-              <RansomNote class="title" text="Привет!" />
-              <RansomNote class="title" text="Я Вова Серёгин" />
-              <p>Я — «человек-немного».</p>
-              <p>Немного программирую на python (django) и javascript (vue.js). Буквы сверху — работа моего <a href="https://github.com/heaviss/ransomnote">ransomnote</a>.</p>
-              <br>
-              <p>Немного сочиняю музыку, учусь играть на гитаре и губной гармошке.</p>
-              <br>
-              <p>Немного пишу (в <a href="https://t.me/rdpms">телеграм-канал</a>, занял 9 место на <a href="https://challenge.glvrd.ru/mtv.html">вызове Главреда</a>).</p>
-              <br>
-              <p>Много читаю, потому что люблю (45 книг в 2016, <a href="https://telegra.ph/thisage--26-02-19">31 в 2017</a>). Читаю про разработку, дизайн, редактуру, устройство людей, рациональное мышление и сочинение музыки. И фантастику.</p>
-              <br>
-              <p>Веду(медленно веду) с Верой Иноземцевой <a href="https://t.me/listen2it">канал «Слушай чё»</a>, где она читает вслух клёвые статьи.</p>
-              <br>
-              <p>Работал безопасником в ЦФТ, сейчас — программирую в ГдеМатериале.</p>
-            </div>
-            <footer class="card-footer">
-              <p class="card-footer-item">
-                <span>
-                  <a href="https://t.me/heavis">Telegram</a>
-                </span>
-              </p>
-              <p class="card-footer-item">
-                <span>
-                  <a href="https://github.com/heaviss/">Github</a>
-                </span>
-              </p>
-            </footer>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <article class="homepage">
+    <RansomNote class="homepage__hi ransomenote" text="Привет!" />
+    <img class="homepage__photo" src="~assets/photo.png" alt="Где фотка, ёпта?">
+    <RansomNote class="homepage__name ransomenote" text="Я Вова Серёгин" />
+    <section class="homepage__about">
+      <p>Я — «человек-немного».</p>
+      <p>Немного программирую на python (django) и javascript (vue.js). Буквы сверху — работа моего <a href="https://github.com/heaviss/ransomnote">ransomnote</a>.</p>
+      <p>Немного сочиняю музыку, учусь играть на гитаре и губной гармошке.</p>
+      <p>Немного пишу (в <a href="https://t.me/rdpms">телеграм-канал</a>, занял 9 место на <a href="https://challenge.glvrd.ru/mtv.html">вызове Главреда</a>).</p>
+      <p>Много читаю, потому что люблю (45 книг в 2016, <a href="https://telegra.ph/thisage--26-02-19">31 в 2017</a>). Читаю про разработку, дизайн, редактуру, устройство людей, рациональное мышление и сочинение музыки. И фантастику.</p>
+      <p>Веду(медленно веду) с Верой Иноземцевой <a href="https://t.me/listen2it">канал «Слушай чё»</a>, где она читает вслух клёвые статьи.</p>
+      <p>Работал безопасником в ЦФТ, сейчас — программирую в ГдеМатериале.</p>
+    </section>
+
+    <section class="homepage__contacts">
+      <p class="card-footer-item">
+        <a href="https://t.me/heavis">Telegram</a>
+      </p>
+      <p class="card-footer-item">
+        <a href="https://github.com/heaviss/">Github</a>
+      </p>
+    </section>
+  </article>
 </template>
 
 <script>
@@ -53,12 +33,67 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style>
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
 }
 
 .links {
   padding-top: 15px;
+}
+
+.homepage {
+  font-size: 1em;
+
+  width: 100vw;
+  height: 100vh;
+}
+
+@media (width > 1024px) {
+  .homepage {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-auto-rows: 1fr;
+  }
+}
+
+.ransomenote {
+  font-size: 1.5em;
+}
+
+@media (width > 1024px) {
+  .ransomenote {
+    font-size: 2em;
+  }
+}
+
+.homepage__hi {
+  grid-column: 3 / 5;
+  grid-row: 3 / 4;
+}
+
+.homepage__photo {
+  grid-column: 4 / 8;
+  grid-row: 2 / 8;
+  max-width: 100%;
+}
+
+.homepage__name {
+  grid-column: 7 / 9;
+  grid-row: 5 / 6;
+}
+
+.homepage__about {
+  grid-column: 4 / 8;
+  grid-row: 9 / 13;
+}
+
+.homepage__about p {
+  margin-bottom: 1em;
+}
+
+.homepage__contacts {
+  grid-column: 4 / 8;
+  grid-row: 14 / 15;
 }
 </style>
