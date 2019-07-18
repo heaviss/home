d98454b29fc9c01f4ac6d915eb4b6ba6a374a26f
Vladimir Seregin <31631@rambler.ru>
2019-07-18 15:10:31 +0700

    Run lint

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
   <section class="section">
     <div class="container">
-    <div class="columns is-centered">
-      <div class="column is-6">
-        <div class="card">
+      <div class="columns is-centered">
+        <div class="column is-6">
+          <div class="card">
             <div class="card-image">
               <figure class="image is-4by3">
                 <img src="~assets/photo.png" alt="Где фотка, ёпта?">
               </figure>
             </div>
             <div class="card-content">
-              <RansomNote class="title" text='Привет!'/>
-              <RansomNote class="title" text='Я Вова Серёгин'/>
-              <p>Я — «немного».</p>
+              <RansomNote class="title" text="Привет!" />
+              <RansomNote class="title" text="Я Вова Серёгин" />
+              <p>Я — «человек-немного».</p>
               <p>Немного программирую на python (django) и javascript (vue.js). Буквы сверху — работа моего <a href="https://github.com/heaviss/ransomnote">ransomnote</a>.</p>
               <br>
               <p>Немного сочиняю музыку, учусь играть на гитаре и губной гармошке.</p>
@@ -21,9 +21,9 @@
               <br>
               <p>Много читаю, потому что люблю (45 книг в 2016, <a href="https://telegra.ph/thisage--26-02-19">31 в 2017</a>). Читаю про разработку, дизайн, редактуру, устройство людей, рациональное мышление и сочинение музыки. И фантастику.</p>
               <br>
-              <p>Веду с Верой Иноземцевой <a href="https://t.me/listen2it">канал «Слушай чё»</a>, где она читает вслух клёвые статьи.</p>
+              <p>Веду(медленно веду) с Верой Иноземцевой <a href="https://t.me/listen2it">канал «Слушай чё»</a>, где она читает вслух клёвые статьи.</p>
               <br>
-              <p>Сейчас работаю безопасником в ЦФТ.</p>
+              <p>Работал безопасником в ЦФТ, сейчас — программирую в ГдеМатериале.</p>
             </div>
             <footer class="card-footer">
               <p class="card-footer-item">
@@ -54,13 +54,11 @@ export default {
 </script>
 
 <style lang="scss">
-.title
-{
+.title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
 }
 
-.links
-{
+.links {
   padding-top: 15px;
 }
 </style>
